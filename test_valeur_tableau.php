5b2dfcc9c0cc64b6ba6f00dc72f32e3b7f022e45
Pascal <andripascal@gmail.com>
2017-02-15 12:10:35 +0300

    Create test_valeur_tableau.php

---
+++ test_valeur_tableau.php
@@ -0,0 +1,7 @@
+	$valeur_id=$id; //valeur test
+	$tableau_autorise = array(6,123,12,...);
+	if (in_array($valeur_id,$tableau_autorise)) {		
+     $valider = $valeur_id; //autoriser tableau
+	}else{		
+	$valider = '6'; //si valeur standard  
+	}
